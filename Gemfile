143ddf5898169f1c31d4bf65dd2f19cd8d09936c
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-02-12 10:11:06 -0500

    chapter 5

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.0'
+gem 'sqlite3'
+
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'bootstrap-sass', '2.3.2'
+
+group :assets do
+  gem 'sass-rails', '~> 5.0'
+  gem 'uglifier', '>= 1.3.0'
+  gem 'coffee-rails', '~> 4.1.0'    
+end
+
+group :development, :test do
+  gem 'rspec-rails', '2.14.2'
+  gem 'capybara', '2.2.1'
+end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_title("Ruby on Rails Tutorial Sample App")
+    end    
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_title("Ruby on Rails Tutorial Sample App | Help")
+    end        
+  end  
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end            
+  end    
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end  
+end
