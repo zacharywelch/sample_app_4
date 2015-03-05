72f101514e8e17162acb6b5eb3e5e4045162c984
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-03-04 21:53:12 -0500

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -6,11 +6,17 @@ gem 'sqlite3'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'bootstrap-sass', '2.3.2'
+gem 'bcrypt', '~> 3.1.0'
+gem 'protected_attributes', '1.0.8'
 
 group :assets do
   gem 'sass-rails', '~> 5.0'
   gem 'uglifier', '>= 1.3.0'
   gem 'coffee-rails', '~> 4.1.0'    
+end
+
+group :development do
+  gem 'annotate', '2.6.5'
 end
 
 group :development, :test do
---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password  
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  before_save { |user| user.email = email.downcase }                    
+end
