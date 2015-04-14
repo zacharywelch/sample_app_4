5eadaadbc499c11f28de515a32c8eed205895204
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-04-14 09:21:40 -0400

    chap 8 finished

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,4 +12,13 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete :remember_token
+  end
 end
