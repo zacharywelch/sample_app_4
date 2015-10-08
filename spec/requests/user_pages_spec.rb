21ce752a05df0d793056eb76ef9f92664dde965a
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-10-07 23:03:32 -0400

    finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "User pages" do
+
+  subject { page }
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  describe "signup page" do
+    
+    before { visit signup_path }
+
+    it { should have_selector('h1', text: 'Sign up') }
+    it { should have_title("#{base_title} | Sign up") }
+  end
+end
