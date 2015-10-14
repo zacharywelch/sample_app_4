337b6fd0b10a4946f8e110753a0da97ba67b1a23
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-10-14 17:02:31 -0400

    6.1.2

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'spec_helper'
+
+describe User do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
