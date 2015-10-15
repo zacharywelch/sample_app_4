1485431f9378831143f680232f3c0d1a19a22826
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-10-14 20:35:08 -0400

    6.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,11 @@
 #
 
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  before_save { |user| user.email = email.downcase }
 end
