bb20632e438d730e73a10d36373f8d8c5e75cdad
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-11-04 23:48:32 -0500

    chap 8

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,7 +14,7 @@ describe "User pages" do
 
   describe "profile page" do    
     
-    let(:user) { FactoryGirl.create(:user) }
+    let(:user) { create :user }
     before { visit user_path(user) }
 
     it { should have_selector('h1', text: user.name) }
@@ -44,6 +44,15 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end      
     end
   end  
 end
